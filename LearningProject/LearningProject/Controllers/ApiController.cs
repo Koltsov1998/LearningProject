6908265f9ca7534211e04682326f4c883e5dd1b0
koltsov.S <koltsov@itglobal.ru>
2020-02-16 16:09:38 +0300

    добавлен сервис для работы с пабликами и метод апи для получения всех пабликов

---
+++ LearningProject/LearningProject/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using LearningProject.API.Contracts;
+using LearningProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningProject.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly IVkPublicService _vkPublicService;
+
+        public ApiController(IVkPublicService vkPublicService)
+        {
+            _vkPublicService = vkPublicService;
+        }
+
+        [HttpGet]
+        [Route("~/api/publics")]
+        [ProducesResponseType(typeof(VkPublicContract[]), 200)]
+        public IActionResult GetVkPublics()
+        {
+            var publics = _vkPublicService.GetAllVkPublics();
+            return Ok(publics);
+        }
+    }
+}
---
+++ LearningProject/LearningProject/Services/VkPublicService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LearningProject.API.Contracts;
+using LearningProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningProject.Services
+{
+    public class VkPublicService : IVkPublicService
+    {
+        private readonly DataContext _context;
+        public VkPublicService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<VkPublicContract> GetAllVkPublics()
+        {
+            return _context.VkPosts
+                .Include(p => p.VkPublic)
+                .GroupBy(p => p.VkPublic)
+                .Select(g => new VkPublicContract
+                {
+                    Uri = g.Key.Uri,
+                    PostsParsed = g.Count()
+                });
+        }
+    }
+}
