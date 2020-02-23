44268c0f8cd30169d8e7c0c28561493fcc59220e
Eveline <monsterelle9@gmail.com>
2020-02-23 22:29:01 +0300

    Добавлена библиотека material ui, улучшен дизайн

--- LearningProject/LearningProject/ClientApp/src/components/publicsComponent.js
+++ LearningProject/LearningProject/ClientApp/src/components/publicsComponent.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Api from '../Utils/Api';
-
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 
 export class Publics extends Component {
     constructor(props) {
@@ -51,37 +60,49 @@ export class Publics extends Component {
                         Список пабликов
                     </div>
                     <div>
-                        <input type="text" value={this.state.publicUrl} onChange={this.handleChange} />
-                        <button onClick={this.handleSubmit}>
+                        <TextField id="standard-basic" label="Ссылка на паблик" value={this.state.publicUrl} onChange={this.handleChange} />
+                        <Button onClick={this.handleSubmit}>
                             Добавить
-                        </button>
+                        </Button>
                     </div>
                 </div>
                 <div>
-                    <table>
-                        {this.state.publics.map((p) =>
-                            <tr>
-                                <td>
-                                    <div>
-                                        <img src={p.photo50}></img>
-                                    </div>
-                                </td>
-                                <td>
-                                    <a href={p.url} target={"_blank"} >{p.name}</a>
-                                </td>
-                                <td>
-                                    {p.postsParsed}
-                                </td>
-                                <td>
-                                    <div>
-                                        <button onClick={this.handleRemove(p.uri)}>
-                                            Удалить
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-                        )}
-                    </table>
+                    <TableContainer component = {Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell></TableCell>
+                                    <TableCell>Название</TableCell>
+                                    <TableCell>Обработано мемов</TableCell>
+                                    <TableCell></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {this.state.publics.map((p) =>
+                                <TableRow>
+                                    <TableCell>
+                                        <div>
+                                            <img src={p.photo50}></img>
+                                        </div>
+                                    </TableCell>
+                                    <TableCell>
+                                        <a href={p.url} target={"_blank"} >{p.name}</a>
+                                    </TableCell>
+                                    <TableCell>
+                                        {p.postsParsed}
+                                    </TableCell>
+                                    <TableCell>
+                                        <div>
+                                            <button onClick={this.handleRemove(p.uri)}>
+                                                Удалить
+                                            </button>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </div>
             </div>
         );
