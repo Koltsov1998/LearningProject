5769226e8c7a1d9a8e1b0086cfe12ed2e07a8aa7
Eveline <monsterelle9@gmail.com>
2020-04-01 20:30:19 +0300

    Страница показывает мемы из паблика

---
+++ LearningProject/LearningProject/API/Contracts/ParsedMemeContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningProject.API.Contracts
+{
+    public class ParsedMemeContract
+    {
+        public int Id { set; get; }
+
+        public string Url { set; get; }
+
+        public string Text { get; set; }
+    }
+}
--- LearningProject/LearningProject/Controllers/ApiController.cs
+++ LearningProject/LearningProject/Controllers/ApiController.cs
@@ -1,16 +1,24 @@
 ﻿using LearningProject.API.Contracts;
 using LearningProject.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace LearningProject.Controllers
 {
     public class ApiController : Controller
     {
         private readonly IVkPublicService _vkPublicService;
+        private readonly MemeParserService _memeParserService;
+        private readonly IMemeManagerService _memeManagerService;
+        private readonly ProgressProcessStorage _progressProcessStorage;
 
-        public ApiController(IVkPublicService vkPublicService)
+        public ApiController(IVkPublicService vkPublicService, MemeParserService memeParserService, 
+            IMemeManagerService memeManagerService, ProgressProcessStorage progressProcessStorage)
         {
             _vkPublicService = vkPublicService;
+            _memeParserService = memeParserService;
+            _memeManagerService = memeManagerService;
+            _progressProcessStorage = progressProcessStorage;
         }
 
         [HttpGet]
@@ -32,10 +40,46 @@ namespace LearningProject.Controllers
 
         [HttpDelete]
         [Route("~/api/publics")]
-        public IActionResult RemovePublic([FromBody] string publicUrl)
+        public IActionResult RemovePublic([FromBody] int id)
         {
-            _vkPublicService.RemovePublic(publicUrl); 
+            _vkPublicService.RemovePublic(id); 
             return Ok();
+        }
+        
+        [HttpPost]
+        [Route("~/api/memes/startparsing")]
+        public  IActionResult StartPhotosParsing([FromBody] int publicId)
+        {
+            _memeParserService.StartParsingPhotosFromPublic(publicId);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("~/api/memes/callformemes")]
+        [ProducesResponseType(typeof(ParsedMemeContract[]), 200)]
+        public IActionResult CallForMemes([FromBody] int publicId)
+        {
+            var memes = _memeManagerService.GetParsedMemes(publicId);
+            return Ok(memes);
+        }
+
+        [HttpGet]
+        [Route("~/api/memes/{publicId}/progress")]
+        [ProducesResponseType(typeof(Progress), 200)]
+        public IActionResult GetProgress(int publicId)
+        {
+            if(_progressProcessStorage.Progresses.ContainsKey(publicId))
+            {
+                var progress = _progressProcessStorage.Progresses[publicId];
+                return Ok(progress);
+            }
+            else
+            {
+                return Ok(new Progress(0)
+                {
+                    Done = 0
+                });
+            }
         }
     }
 }
